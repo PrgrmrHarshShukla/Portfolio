28eced0a2f50648e36c94df178444b283b3e7f43
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-08-05 16:58:21 +0530

    Create ProjectData.jsx

---
+++ client/src/components/ProjectData.jsx
@@ -0,0 +1,32 @@
+export default [
+   {
+      name: "S-Mart",
+      live: "https://s-mart-e-commerce.netlify.app/",
+      repo: "https://github.com/PrgrmrHarshShukla/S-Mart",
+      desc: "This web app has full fledged e-commerce functionality. I made it using ReactJS & TailwindCSS, firebase authentication, etc.",
+   },
+   {
+      name: "FireChat",
+      live: "https://s-mart-e-commerce.netlify.app/",
+      repo: "https://github.com/PrgrmrHarshShukla/FireChat",
+      desc: "This is a real-time chat app. I made this using ReactJS, TailwindCSS and Socket.io.",
+   },
+   {
+      name: "Tracker",
+      live: "https://upcoming-coding-contests.netlify.app/",
+      repo: "https://github.com/PrgrmrHarshShukla/Upcoming-Coding-Contests",
+      desc: "This website gives a list of the upcoming coding contests, alongwith the date, starting time and organising site url.",
+   },
+   {
+      name: "Tic-Tac-Toe",
+      live: "https://legacy-tic-tac-toe-web-app.netlify.app/",
+      repo: "https://github.com/PrgrmrHarshShukla/Tic-Tac-Toe-Game",
+      desc: "This is the legacy Tic-Tac-Toe Game. I made it using the basic concepts of ReactJS.",
+   },
+   {
+      name: "Timer",
+      live: "https://count-down-timer-web-app.netlify.app/",
+      repo: "https://github.com/PrgrmrHarshShukla/CountDown-Timer",
+      desc: "This is a count-down timer, made using HTML, CSS & JS. It takes an event from the user and runs a countdown to it.",
+   },
+]
