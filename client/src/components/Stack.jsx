aaa020246e974068ebe5b760b712ba1e7e0872ba
_ HARSH _ <ashuklaharsh2005@gmail.com>
2024-08-03 09:17:02 +0530

    fix: tech stack

--- client/src/components/Stack.jsx
+++ client/src/components/Stack.jsx
@@ -5,15 +5,15 @@ function Stack(props) {
     <div className="min-h-[500px] sm:min-h-[370px] w-screen px-8 flex flex-col sm:flex-row pb-[100px] mt-8">
 
       <div className={`min-h-[100px] sm:h-auto sm:w-1/3 text-3xl flex flex-row justify-center items-start ${props.isDark ? "text-white " : "text-black "} font-semibold`}>
-         <p>Skill set</p>
+         <p>Tech Stack</p>
       </div>
 
       <div className={`sm:w-2/3 sm:max-w-[500px] ${props.isDark ? "text-white " : "text-black "} flex flex-col justify-start gap-8 sm:gap-4 text-[15px] sm:text-lg`}>
         <p>
-          Next.js, React.js, TypeScript, JavaScript, TailwindCSS, Semantic HTML, CSS, MongoDB & Atlas, PostgreSQL, Prisma
+          Next.js, React.js, TypeScript, JavaScript, TailwindCSS, PostgreSQL, Prisma ORM, Node.js, Express.js, MongoDB & Atlas, Semantic HTML, CSS
         </p>  
         <p>
-          Firebase<i> (Backend as a service)</i>, Prettier, EsLint, Terminal, Version Control <i>(Git)</i>, Vercel, Netlify, Render <i>(Hosting service)</i>, Analytics
+          Firebase, Version Control <i>(Git)</i>, Vercel, Netlify, Render <i>(Hosting service)</i>, Analytics
         </p>  
         <p>
           Python, C/C++
