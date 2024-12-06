1cb83bc04ed7439742bb40102bcf001f44fa5b69
HARSH S <ashuklaharsh2005@gmail.com>
2024-12-06 21:48:52 +0530

    add: Stack

--- client/src/components/Stack.jsx
+++ client/src/components/Stack.jsx
@@ -10,17 +10,23 @@ function Stack(props) {
 
       <div className={`sm:w-2/3 sm:max-w-[490px] ${props.isDark ? "text-white " : "text-black "} flex flex-col justify-start gap-8 sm:gap-4 text-[15px] sm:text-lg`}>
         <p>
-          Next.js, React.js, TypeScript, JavaScript, TailwindCSS, PostgreSQL, Prisma ORM, Node.js, Express.js, MongoDB & Atlas, Semantic HTML, CSS
-        </p>  
-        <p>
-          Firebase, Version Control <i>(Git)</i>, Vercel, Netlify, Render <i>(Hosting service)</i>, Analytics
-        </p>  
-        <p>
-          Python, C/C++
+          <strong className="underline">Tech Stack (Web)</strong>:  Next.js, React.js, Node.js, Express.js, Django
         </p>
-        {/* <p>
-          On my soft side, I am a responsible-careful-creative-strong-willed-detail-oriented-optimistic-ever-learning-polite and hard-working person. 
-        </p>  */}
+        <p>
+          <strong className="underline">Tech Stack (Android)</strong>:  React Native and Expo
+        </p>
+        <p>
+          <strong className="underline">Styling</strong>:  TailwindCSS, ShadCN, ChakraUI
+        </p>
+        <p>
+          <strong className="underline">Languages</strong>:  JavaScript, TypeScript, C/C++, Python
+        </p>
+        <p>
+          <strong className="underline">Databases</strong>:  PostgreSQL, MongoDB
+        </p>
+        <p>
+          <strong className="underline">Miscellaneous</strong>:  Prisma, Vercel, Netlify, Leetcode, Codeforces, Firebase, Render, Git, GitHub, AWS, Docker, RESTful APIs, GraphQL, WebSockets, OAuth, JWT
+        </p>
                      
       </div>
 
